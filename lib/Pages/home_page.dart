d3137881d867928866785333d3c2707f7deac86f
AjaxxIsHere <ajaz2468@gmail.com>
2023-02-06 19:32:29 +0530

    added tmdb api, just need to create homepage

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,9 +1,7 @@
 // ignore_for_file: camel_case_types
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nextmovie_v2/Pages/signin_page.dart';
+import 'package:tmdb_api/tmdb_api.dart';
 
 class homescreenPage extends StatefulWidget {
   const homescreenPage({super.key});
@@ -13,23 +11,50 @@ class homescreenPage extends StatefulWidget {
 }
 
 class _homescreenPageState extends State<homescreenPage> {
+  List trendingMovies = [];
+  List topRatedMovies = [];
+  List tvShows = [];
+  final String tmdbAPIkey = 'ed16e80fbc7853efc31460a41a8575bd';
+  final readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDE2ZTgwZmJjNzg1M2VmYzMxNDYwYTQxYTg1NzViZCIsInN1YiI6IjYzZGU1NWJiY2QyMDQ2MDA4MWUxYjhmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ej5mgpEQekD3RIlt9wOyMYoZclEjfKzLM4z_M4mv8QI';
+
+  @override
+  void initState() {
+    loadmovies();
+    super.initState();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(tmdbAPIkey, readaccesstoken),
+        logConfig: const ConfigLogger(
+          showLogs: true,
+          showErrorLogs: true,
+        ));
+
+    Map TrendingResults = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topRatedResults = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvResults = await tmdbWithCustomLogs.v3.tv.getPopular();
+    //print(TrendingResults);
+    //return TrendingResults['results'];
+    setState(() {
+      trendingMovies = TrendingResults['results'];
+      topRatedMovies = topRatedResults['results'];
+      tvShows = tvResults['results'];
+    });
+    print(TrendingResults);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Next Movie")),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut().then((value) {
-                print("Signed Out");
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const signinScreen()));
-              });
-            },
-            child: const Text("Logout")),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Next Movie")),
+        body: const Center(
+          child: Text("placeholder"), // LEFT OFF HERE
+        ),
       ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.green),
     );
   }
 }
